52983358412598f53c2bbcce7edaea413426cba7
kudsophia <99794475+kudsophia@users.noreply.github.com>
2024-02-23 10:32:00 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input a:");
+        double a = double.Parse(Console.ReadLine());
+        
+        double radius = a / ( 2 * Math.Sqrt(3));
+
+        Console.WriteLine($"Radius = : {radius}");
+    }
+}
