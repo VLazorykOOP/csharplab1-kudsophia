61407ee19e9bab9a58d0d8e5eb20bb147cea498d
kudsophia <99794475+kudsophia@users.noreply.github.com>
2024-02-23 10:38:22 +0200

    Program1.cs

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -13,4 +13,15 @@
 }
 Console.WriteLine("Lab 1 !");
 task1();
-// continue ...
+class Program1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input a:");
+        double a = double.Parse(Console.ReadLine());
+        
+        double radius = a / ( 2 * Math.Sqrt(3));
+
+        Console.WriteLine($"Radius = : {radius}");
+    }
+}
